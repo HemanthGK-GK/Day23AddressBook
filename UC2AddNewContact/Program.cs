853b7a7d2e3b2bf0c03e1024a3a42db9f01f9974
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-14 18:40:35 +0530

    Hemanth:Adding contact to Address Book

---
+++ UC2AddNewContact/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UC2AddNewContact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Details ob = new Details();
+            ob.choice();
+        }
+    }
+}
