f6713aac1e173bc6d8344333c717c54067c291c9
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-14 18:41:59 +0530

    Hemanth :Edit the old entries in Address Book

---
+++ UC3EditContact/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UC3EditContact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Details ob = new Details();
+            ob.choice();
+        }
+    }
+}
